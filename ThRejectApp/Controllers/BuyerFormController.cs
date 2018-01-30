89cbe7d01ee25c2e12d8cac8e2b0f5d4d1b0403b
Chris Franks <1000frankschris@gmail.com>
2018-01-30 21:27:58 +0000

    Buyer form loads real data from db

--- ThRejectApp/Controllers/BuyerFormController.cs
+++ ThRejectApp/Controllers/BuyerFormController.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ThRejectApp.Models;
 
 namespace ThRejectApp.Controllers
 {
     public class BuyerFormController : Controller
     {
         // GET: BuyerForm
-        public ActionResult EditForm(string RejectNo)
+        public ActionResult EditForm(int RejectNo)
         {
-            ViewBag.RejectNo = RejectNo;
-            return View();
+            ViewBag.RejectNo = RejectNo.ToString();
+
+            RejectDbEntities dbConnection = new RejectDbEntities();
+
+            tbRejectRecord RejectRecord = dbConnection.tbRejectRecords.Find(RejectNo);
+
+            return View(RejectRecord);
         }
     }
 }
